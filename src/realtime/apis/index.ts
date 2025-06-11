5938dbc9a7d5e29887d9641f0e39ef8800445d97
mousecrusher2 <95418154+mousecrusher2@users.noreply.github.com>
2025-06-11 23:13:08 +0900

    Remove obsolete realtime models and runtime code from the project, including RenegotiateResponse, SessionDescription, TrackObject, TrackObjectSimulcast, TracksRequest, TracksResponse, TracksResponseTracksInner, UpdateTracksRequest, UpdateTracksResponse, and the runtime configuration. This cleanup reduces code complexity and maintenance overhead.

--- src/realtime/apis/index.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './AddATrackApi';
-export * from './ChangeTracksApi';
-export * from './CloseATrackApi';
-export * from './GetSessionStateApi';
-export * from './NewSessionApi';
-export * from './RenegotiateWebRTCSessionApi';
