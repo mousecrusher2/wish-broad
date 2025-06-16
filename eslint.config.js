812dc3d56dfcc0170c3583fd2078eedaffbe5b34
mousecrusher2 <95418154+mousecrusher2@users.noreply.github.com>
2025-06-16 09:44:29 +0900

    fix: ESLint設定を更新し、react-compilerプラグインを追加

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import reactCompiler from "eslint-plugin-react-compiler"
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -10,15 +11,18 @@ export default tseslint.config(
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: "latest",
       globals: globals.browser,
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'react-compiler': reactCompiler,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...reactRefresh.configs.recommended.rules,
+      ...reactCompiler.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
