afe7a0a957e69a9f8cafc35ae52fa637502cc4a9
mousecrusher2 <95418154+mousecrusher2@users.noreply.github.com>
2026-06-13 03:56:24 +0900

    chore: disable react doctor async parallel rule

--- src/player/WHEPClient.ts
+++ src/player/WHEPClient.ts
@@ -780,7 +780,6 @@ export class WHEPSession {
         });
       }
 
-      // react-doctor-disable-next-line react-doctor/async-parallel
       const localOffer = await this.pc.createOffer();
       await this.pc.setLocalDescription(localOffer);
       // The current Calls-backed Worker path is effectively non-trickle. Send a
